544075cca4f2b4797398dac3159afb27c92c978d
ursamaior <superfastblitzmann@gmail.com>
2019-07-23 20:29:53 +0200

    fix travis build

--- src/index.js
+++ src/index.js
@@ -7,18 +7,20 @@ import View from 'ol/View.js'
 import TileLayer from 'ol/layer/Tile.js'
 import OSM from 'ol/source/OSM.js'
 
+// this file should only combine the various parts of the system, nothing else
+
 // TODO put this to some file and import map
-var map = new Map({
+const map = new Map({
   layers: [
     new TileLayer({
       source: new OSM()
     })
   ],
-  target: 'map-hook',
+  target: 'boss',
   view: new View({
     center: [0, 0],
     zoom: 2
   })
 })
 
-console.log('This is nice.')
+console.log('This is nice.', map)
